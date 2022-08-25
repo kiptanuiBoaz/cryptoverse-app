679eec730f1bdf312d3e1c5bb761bca2be086152
Boaz <boaserem022@gmail.com>
2022-08-25 19:45:13 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Switch, Link,Route } from "react-router-dom";
+import {Layout, Typography,space } from "antd";
 
 const App = () => {
   return (
-    <p>microverse</p>
+        <div className="app">
+            <div className="navbar">
 
+            </div>
+
+            <div className="main">
+
+            </div>
+
+            <div className="footer">
+                
+            </div>
+        </div>
 
   )
 }
