6a4075a33c0aacbd52b12b5cd20b5f0e9954eee7
Boaz <boaserem022@gmail.com>
2022-08-26 13:59:12 +0300

    04

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDom from "react-dom";
 import App from "./App";
 import {BrowserRouter as Router }  from "react-router-dom";
+import "antd/dist/antd.css";
 
 ReactDom.render(
 
