bc76f4b8897e72b5841633566dcc09397d426b00
Boaz <boaserem022@gmail.com>
2022-09-14 16:25:27 +0300

    git commit

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -22,8 +22,8 @@ export const Homepage = () => {
       <Row>
       <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats?.total} /></Col>
         <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats?.totalExchanges)} /></Col>
-        <Col span={12}><Statistic title="Total Market Cap:" value={`$${millify(globalStats?.totalMarketCap)}`} /></Col>
-        <Col span={12}><Statistic title="Total 24h Volume" value={`$${millify(globalStats?.total24hVolume)}`} /></Col>
+        <Col span={12}><Statistic title="Total Market Cap:" value={millify(globalStats?.totalMarketCap)} /></Col>
+        <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats?.total24hVolume)} /></Col>
         <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats?.total} /></Col>
         <Col span={12}><Statistic title="Total Markets" value={millify(globalStats?.totalMarkets)} /></Col>
       </Row>
